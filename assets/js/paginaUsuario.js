435908bbcad75f1b02bf833401f03bceec271ad7
CDavid07 <73976024+CDavid07@users.noreply.github.com>
2022-08-25 10:43:33 -0500

    paginaUsuario

--- assets/js/paginaUsuario.js
+++ assets/js/paginaUsuario.js
@@ -1,12 +1,24 @@
-const url = "assets/sugerencias.json"
 
+const url = "../assets/sugerencias.json"
 const imgIcono = document.getElementById("img-icono");
 let max = 10;
 let min = 1;
-
+let despliegue = true;
 let icono = Math.random() * (max-min) + min;
+
 icono = parseInt(icono);
-imgIcono.src = `assets/images/icono/${icono}.png` 
+imgIcono.src = `../assets/images/icono/${icono}.png` 
+
+const imgs = document.querySelectorAll('.img');
+imgs.forEach(img=>{
+    img.addEventListener('click', function(){
+        
+        const select = document.querySelector('.seleccionado');   
+        select.classList.remove('seleccionado');
+        img.classList.add('seleccionado');
+       
+    })
+})
 
 async function obtenerSugerencias(url) {
     try {
@@ -19,4 +31,14 @@ async function obtenerSugerencias(url) {
     }
 }
 
+function borrar() {
+    const active = document.querySelector('.active')
+    console.log(active.textContent);
+}
+
+function actualizar(){
+    const select = document.querySelector('.seleccionado');
+    const imgIcono = document.getElementById("img-icono");
+    imgIcono.src = select.src
+}
 obtenerSugerencias(url);
