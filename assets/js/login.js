2f79c8ffdeb9f20fcaf99e2599d960a24dd8434f
Rosy <109691073+Rosyalba@users.noreply.github.com>
2022-09-01 15:05:51 -0500

    Merge branch 'main' into PaginaLogin1

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,8 +3,13 @@
 
 
 //Funcion Steffy
+
+// function validar(){
+//     window.location.href ='./paginaSesionInicio.
+
 // function validar(){
 //     window.location.href ='./paginaSesionInicio.html';
+
 
 // var user = document.getElementsById("username").value;
 // var pass= document.getElementsById("password").value;
