dc3d4d29cc88dfa162155ae81f374136862f19f0
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-09-06 22:25:25 +0100

    formularios
    responsive design - formularios

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,20 +1,32 @@
+/*
+var nombre = document.getElementById('nombre');
+var password = document.getElementById('password');
+var error = document.getElementById('error');
+
+function enviarForm(){
+    console.log("Enviando formulario");
+    
+
+    error.innerHTML =  mensajesError.join(', ');
+    return false;
+}*/
+var form = document.getElementById('formulario');
+form.addEventListener('submit',function(evt)){
+evt.preventDefault();
+var mensajesError=[];
+var mensajesError =[];
+    if(nombre.value===null || nombre.value==='' || nombre.value===' '){
+        mensajesError.push('Ingresa tu usuario');
+    }
+    if(password.value===null || password.value ===''){
+        mensajesError.push('Ingresa tu Password');
+    }
+console.log('Enviando Formulario...')
+}
+
+
 
 //var button = document.getElementById('button')
 
 
 
-function validar(){
-    window.location.href ='../pages/paginaSesionInicio.html';
-
-var user = document.getElementsById("username").value;
-var pass= document.getElementsById("password").value;
-  
-    if(user == "hola" && pass == "hola"){
-       function hola(){
-        window.location.href ='../pages/paginaSesionInicio.html';
-       } 
-      
-    }else{
-        alert("verifique los datos")
-    }
-}
