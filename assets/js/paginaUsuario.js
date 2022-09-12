71894483ca0a1093329dcaa20e6701daad1003cd
CDavid07 <73976024+CDavid07@users.noreply.github.com>
2022-09-11 21:25:50 -0500

    mensajesUsuario

--- assets/js/paginaUsuario.js
+++ assets/js/paginaUsuario.js
@@ -1,5 +1,7 @@
 
 const url = "../assets/sugerencias.json"
+const url2 = "http://localhost:8080/pacientesR"
+
 const imgIcono = document.getElementById("img-icono");
 let max = 10;
 let min = 1;
@@ -24,11 +26,57 @@ async function obtenerSugerencias(url) {
     try {
         const respuesta = await fetch(url);
         const dato = await respuesta.json();
-    
+        const msj = document.getElementById("mensaje");
+
+        let mensajes = ["consejos","beneficios","riesgos","cuidados"];
+        let max = 4;
+        let min = 0;
+        let tipo = parseInt(Math.random() * (max-min) + min) ;
+
+        switch (tipo) {
+            case 0:
+                msj.classList.add('bg-success');
+                break;
+            case 1:
+                msj.classList.add('bg-info');
+                break;
+            case 2:
+                msj.classList.add('bg-danger');
+                break;
+            case 3:
+                msj.classList.add('bg-warning');
+                break;
+          }
+
+        max = dato[mensajes[tipo]].length;
+        let mensaje = parseInt(Math.random() * (max-min) + min);
+        console.log(mensajes[tipo]);
+        msj.innerHTML = "";
+        msj.innerHTML += `<h3 class="m-0 text-center py-4 text-white">
+          ${dato[mensajes[tipo]][mensaje].titulo}
+        </h3>
+        <p class="m-0 text-center py-4 text-white">
+          ${dato[mensajes[tipo]][mensaje].informacion}
+        </p>
+      </div> `
+
         
     } catch (error) {
         console.log(error);
     }
+}
+
+function obtenerDatos(){
+
+    fetch("http://localhost:8080/pacientesR", {method: 'GET', mode: 'cors'})
+    .then(respuesta => {
+       return respuesta.json()
+    })
+    .then(data => {
+       
+       console.log(data[0].nombre);
+          
+    })
 }
 
 function borrar() {
@@ -42,3 +90,4 @@ function actualizar(){
     imgIcono.src = select.src
 }
 obtenerSugerencias(url);
+//obtenerDatos();
