d26b00f3dc89b5c179b7f7487dce9e85b31c393a
Luis Diaz <luisantoniodiaz97@gmail.com>
2022-09-20 20:54:53 -0500

    POST y GET de Comentario

--- backend/src/main/java/com/proyecto_Dientes_Limpios/modelo/ComentariosModel.java
+++ backend/src/main/java/com/proyecto_Dientes_Limpios/modelo/ComentariosModel.java
@@ -1,5 +1,52 @@
 package com.proyecto_Dientes_Limpios.modelo;
 
-public class ComentariosModel {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "comentario")
+public class ComentariosModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "nombrep")
+	private String nombreP;
+
+	@Column(name = "texto")
+	private String texto;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getnombreP() {
+		return nombreP;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+	
+	public void setnombreP(String nombreP) {
+		this.nombreP  = texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public ComentariosModel(String nombreP, String texto) {
+		super();
+		this.nombreP = nombreP;
+		this.texto = texto;
+	}
 }
